25c1d5d8530229ead1121415e5ad0d2451c2e2b2
Zharkikh <103017672+Zharkikh@users.noreply.github.com>
2022-12-21 22:19:52 +0600

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,18 +1,21 @@
 # PythonStudy
-Обучение новому языку программирования домашние задания 
+print("Введите входной параметр5")
 n = int(input())
-n *= 2
-for mask in range(1 << n):
-    balance, is_good, answer = 0, True, ''
-    for bit in range(n):
-        if (mask >> bit) & 1:
-            balance += 1
-            answer += '('
-        else:
-            balance -= 1
-            if balance < 0:
-                is_good = False
-                break
-            answer += ')'
-    if is_good and balance == 0:
-        print(answer)
+if 0<n<10:
+    n *= 2
+    for mask in range(1 << n):
+        balance, is_good, answer = 0, True, ''
+        for bit in range(n):
+            if (mask >> bit) & 1:
+                balance += 1
+                answer += '('
+            else:
+                balance -= 1
+                if balance < 0:
+                    is_good = False
+                    break
+                answer += ')'
+        if is_good and balance == 0:
+            print(answer)
+else:
+    print("Входной параметр должен быть в промежутке от [0.....10]")
